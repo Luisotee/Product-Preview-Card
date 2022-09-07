83f0f5e1635e25b87ae7cadd48c3843d81706b46
Luisotee <luisotaviomgr1@gmail.com>
2022-09-07 18:57:32 -0300

    change backgroud color and some mantine configs

---
+++ components/main-card.tsx
@@ -0,0 +1,5 @@
+import { Card } from '@mantine/core';
+
+export function MainCard() {
+  return <Card style={{ fontFamily: 'Montserrat, sans-serif' }}>aa</Card>;
+}
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,37 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import {
+  ColorScheme,
+  ColorSchemeProvider,
+  MantineProvider,
+} from '@mantine/core';
+import type { AppProps } from 'next/app';
+import Head from 'next/head';
+import { useState } from 'react';
+import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>Planner</title>
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width"
+        />
+
+        <link
+          href="https://fonts.googleapis.com/css2?family=Fraunces:opsz,wght@9..144,100;9..144,400&family=Roboto:wght@400;500;700&display=swap"
+          rel="stylesheet"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Fraunces:opsz,wght@9..144,100;9..144,400&family=Montserrat&family=Roboto:wght@400;500;700&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+      <MantineProvider>
+        <Component {...pageProps} />
+      </MantineProvider>
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
